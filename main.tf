8e506fd494e4e5b8ceccbcbb1c3fe7c9b16934da
amara vasu <amaravasu123@gmail.com>
2024-09-26 16:18:38 +0530

    delete few files

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
-module "resourcegroup" {
-  source = "./modules/resourcegroup"
-  resource_group_name = var.resourcegroup
-  location= var.region
-  
+resource "azurerm_resource_group" "az_rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
 }
