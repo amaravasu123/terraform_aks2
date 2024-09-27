45ef7a737b336b2ddae3de307d315ea4aac5867d
amara vasu <amaravasu123@gmail.com>
2024-09-27 13:38:20 +0530

    vnet added

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ module "vnet"{
     vnet_name = var.vnet_name
     address_space = [var.address_space]
     subnet_name = var.subnet_name
-    location = module.resource-group.resource_location
-    resource_group_name = module.resource-group.resource_group_name
+    location = var.location
+    resource_group_name = var.resource_group_name
     
 }
