5b639b7ceac22ec348b2cd9498f65ef31cc09cac
amara vasu <amaravasu123@gmail.com>
2024-09-27 13:22:36 +0530

    vnet added

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "az_vnet" {
   name                = var.vnet_name
-  location            = module.resource-group.location
-  resource_group_name = module.resource-group.resource_group_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
   address_space       = var.address_space
   tags                = merge(var.tags, var.additional_vnet_tags)
 }
